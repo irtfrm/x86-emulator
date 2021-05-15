e168c7dff30f08d0f2d586586d992049bf74b788
irtfrm <frm.tomoya@gmail.com>
2021-05-16 02:33:53 +0900

    complete section 3.4

--- src/instruction.c
+++ src/instruction.c
@@ -1,4 +1,6 @@
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 
 #include "instruction.h"
@@ -34,13 +36,79 @@ static void mov_rm32_r32(Emulator* emu)
     set_rm32(emu, &modrm, r32);
 }
 
-static void mov_r32_rm32(Emulator*emu)
+static void mov_r32_rm32(Emulator* emu)
 {
     emu->eip += 1;
     ModRM modrm;
     parse_modrm(emu, &modrm);
     uint32_t rm32 = get_rm32(emu, &modrm);
     set_r32(emu, &modrm, rm32);
+}
+
+static void add_rm32_r32(Emulator* emu)
+{
+    emu->eip += 1;
+    ModRM modrm;
+    parse_modrm(emu, &modrm);
+    uint32_t r32 = get_r32(emu, &modrm);
+    uint32_t rm32 = get_rm32(emu, &modrm);
+    set_rm32(emu, &modrm, rm32+r32);
+}
+
+static void sub_rm32_imm8(Emulator* emu, ModRM* modrm)
+{
+    uint32_t rm32 = get_rm32(emu, modrm);
+    uint32_t imm8 = (int32_t)get_sign_code8(emu, 0);
+    emu->eip += 1;
+    set_rm32(emu, modrm, rm32 - imm8);
+}
+
+static void code_83(Emulator* emu)
+{
+    emu->eip += 1;
+    ModRM modrm;
+    parse_modrm(emu, &modrm);
+
+    switch (modrm.opecode) {
+        case 5:
+            sub_rm32_imm8(emu, &modrm);
+            break;
+        default:
+            printf("not implemented: 83 /%d\n", modrm.opecode);
+            exit(1);
+    }
+}
+
+static void inc_rm32(Emulator* emu, ModRM* modrm)
+{
+    uint32_t value = get_rm32(emu, modrm);
+    set_rm32(emu, modrm, value + 1);
+}
+
+static void dec_rm32(Emulator* emu, ModRM* modrm)
+{
+    uint32_t value = get_rm32(emu, modrm);
+    set_rm32(emu, modrm, value - 1);
+}
+
+static void code_ff(Emulator* emu)
+{
+    emu->eip += 1;
+    ModRM modrm;
+    parse_modrm(emu, &modrm);
+
+    switch (modrm.opecode)
+    {
+    case 0:
+        inc_rm32(emu, &modrm);
+        break;
+    case 1:
+        dec_rm32(emu, &modrm);
+        break;
+    default:
+        printf("Not implemented: FF /%d\n", modrm.opecode);
+        exit(1);
+    }
 }
 
 static void short_jump(Emulator* emu)
@@ -60,6 +128,8 @@ instruction_func_t* instructions[256];
 void init_instructions(void) {
     int i;
     memset(instructions, 0, sizeof(instructions));
+    instructions[0x01] = add_rm32_r32;
+    instructions[0x83] = code_83;
     instructions[0x89] = mov_rm32_r32;
     instructions[0x8B] = mov_r32_rm32;
     for (i = 0; i < 8; i++) {
@@ -68,4 +138,5 @@ void init_instructions(void) {
     instructions[0xc7] = mov_rm32_imm32;
     instructions[0xE9] = near_jump;
     instructions[0xEB] = short_jump;
+    instructions[0xff] = code_ff;
 }
