00c8376715bda2807dddd056a5d2d57cac1e6d8d
irtfrm <frm.tomoya@gmail.com>
2021-05-20 01:50:45 +0900

    implement EFLAGS i/o

--- src/emulator_function.h
+++ src/emulator_function.h
@@ -4,6 +4,11 @@
 #include "emulator.h"
 #include "modrm.h"
 
+#define CARRY_FLAG (1)
+#define ZERO_FLAG (1 << 6)
+#define SIGN_FLAG (1 << 7)
+#define OVERFLOW_FLAG (1 << 11)
+
 uint32_t get_code8(Emulator* emu, int index);
 uint32_t get_sign_code8(Emulator* emu, int index);
 uint32_t get_code32(Emulator* emu, int index);
@@ -17,5 +22,16 @@ void set_memory32(Emulator* emu, uint32_t address, uint32_t value);
 
 void push32(Emulator* emu, uint32_t value);
 uint32_t pop32(Emulator* emu);
+
+void set_carry(Emulator* emu, int is_carry);
+void set_zero(Emulator* emu, int is_zero);
+void set_sign(Emulator* emu, int is_sign);
+void set_overflow(Emulator* emu, int is_overflow);
+void update_eflags_sub(Emulator* emu, uint32_t v1, uint32_t v2, uint64_t result);
+
+int is_carry(Emulator* emu);
+int is_zero(Emulator* emu);
+int is_sign(Emulator* emu);
+int is_overflow(Emulator* emu);
 
 #endif
